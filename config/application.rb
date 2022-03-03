2df8712ba4950d98383dda4405ed533f3cd05616
Tomoo Fujii <depapepe529@gmail.com>
2022-03-04 05:25:22 +0900

    configに追加

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module SoccerPost
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Tokyo'
-    
+    config.assets.enabled = false
   end
 end
